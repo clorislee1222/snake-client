f3aa5ac31b2286931f85df74cdd25decd3e93380
Cloris Li <cloris.lzy@gmail.com>
2023-02-16 01:34:57 -0800

    Move Commands

--- client.js
+++ client.js
@@ -12,12 +12,18 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //Print a message to the screen when the connection is successfully established.
   conn.on("connect",() => {
-    console.log("Successfully connected!");
+    console.log("Successfully connected to game server");
     conn.write("Name: Clo");
-  }
-  );
+  });
 
+  // //Send the string "Move: up" as data to the server
+  // conn.on("connect",() => {
+  //   conn.write("Move: up");
+  // });
+
+  //Print incoming data
   conn.on("data", (data) => {
     console.log(data);
   })
